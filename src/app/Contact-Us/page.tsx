68ecb39d217c5a7c01cf3e2091449079294cfd3c
Nipuna <k.nipuna.dasun@gmail.com>
2025-03-23 19:37:03 +0530

    chnaged it to puslese

--- src/app/Contact-Us/page.tsx
+++ src/app/Contact-Us/page.tsx
@@ -88,7 +88,7 @@ const ContactUs = () => {
                 </p>
               </div>
               <div className="flex items-center space-x-4">
-              <FaEnvelope className="text-blue-500 text-2xl animate-[shake_0.3s_ease-in-out_infinite]" />
+                <FaEnvelope className="text-blue-500 text-2xl animate-pulse" />
 
                 <p className="text-gray-700 hover:text-blue-600 transition">
                   norwoodlankateasinternational@gmail.com
